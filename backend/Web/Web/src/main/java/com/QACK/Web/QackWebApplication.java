2018e89d465da1057e1b2ef06bddf642d932ed6d
Luis Bussolin <luiscarlosbusso@gmail.com>
2024-08-12 14:20:08 -0300

    Backend: adding backend struct

---
+++ backend/Web/Web/src/main/java/com/QACK/Web/QackWebApplication.java
@@ -0,0 +1,13 @@
+package com.QACK.Web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QackWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QackWebApplication.class, args);
+	}
+
+}
